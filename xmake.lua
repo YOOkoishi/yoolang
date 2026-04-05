cab92f1b92c04eb3c68bf66d6063a4291d371e98
YOO_koishi <2358181935@qq.com>
2026-04-05 20:37:49 +0800

    ✨feat : update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 add_rules("mode.debug", "mode.release")
 
-target("bsb")
+target("yoolang")
     set_kind("binary")
     set_languages("c++17")
-    add_files("src/**.cpp")    
+    add_files("src/**.cpp")
 
